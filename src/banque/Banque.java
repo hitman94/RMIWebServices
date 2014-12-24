97c8fca7670354a73f37261645ab41c6634dac86
Thomas Londechamp <Thomas@Thomas-PC>
2014-12-24 15:17:02 +0100

    Ajout ConcurrentHashmap

---
+++ src/banque/Banque.java
@@ -0,0 +1,32 @@
+package banque;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Banque {
+	private Double solde;
+	private ConcurrentHashMap<String, Double> map;
+
+	public Banque(){
+		this.solde = 0.0;
+		this.map = new ConcurrentHashMap<>();
+	}
+	
+	public void depotDe(String username, double montant){
+		solde += montant;
+		map.put(username, solde);
+	}
+	
+	public boolean retraitDe(String username, double montant){
+		if(solde - montant < 0)
+			return false;
+		else {
+			solde -= montant;
+			map.put(username, solde);
+			return true;
+		}
+	}
+	
+	public double valeurDuSolde(String username){
+		return map.get(username);
+	}
+}
