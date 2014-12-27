0e67b62c48226620f9840d75d28cb203a1f37ab5
Thomas Londechamp <Thomas@Thomas-PC>
2014-12-27 17:36:15 +0100

    On serialize !

--- src/sellingBook/SellingBookWS.java
+++ src/sellingBook/SellingBookWS.java
@@ -1,10 +1,15 @@
 package sellingBook;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import sellingBook.interfaceRMI.IBook;
 import sellingBook.interfaceRMI.ILibraries;
@@ -20,6 +25,60 @@ private ILibraries lib;
 		lib.addBook(new Long("14254414"), "title", "Florian", new Double("124574"));
 		IBook b = lib.getBook(new Long("14254414"));
 		System.out.println(b.getAuthor());
+	}
+	
+public static void serialize(String username, IBook[] basket) {
+		
+		ObjectOutputStream oos = null;
+		
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+		try {
+			File dirUsers = new File("users");
+			dirUsers.mkdir();
+			final FileOutputStream fichier = new FileOutputStream(
+					dirUsers + System.getProperty("file.separator") + username);
+			oos = new ObjectOutputStream(fichier);
+			oos.writeObject(basket);
+			oos.flush();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (oos != null) {
+					oos.flush();
+					oos.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public static IBook[] unserialize(String username) {
+
+		ObjectInputStream ois = null;
+		List<IBook> basket = new ArrayList<IBook>();
+
+		try {
+			final FileInputStream fichier = new FileInputStream(
+					"users" + System.getProperty("file.separator") + username);
+			ois = new ObjectInputStream(fichier);
+			basket = (List<IBook>) ois.readObject();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		} catch (final ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return (IBook[]) basket.toArray();
 	}
 }
 
